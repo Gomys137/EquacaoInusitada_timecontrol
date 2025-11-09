eb3f5f9a88e76059150892b841d1b039176762b0
Daniel <dg185291@gmail.com>
2025-11-09 14:49:47 +0000

    Limpei _app.js e removi marcas de merge

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
-<<<<<<< HEAD
 // pages/_app.js
-import '../styles/globals.css';
+import '@/styles/globals.css';
+import '@/styles/login.css';
+import '@/styles/func_dashboard.css';
 import { AuthProvider } from '../context/AuthContext';
 
 export default function App({ Component, pageProps }) {
@@ -10,26 +11,3 @@ export default function App({ Component, pageProps }) {
     </AuthProvider>
   );
 }
-=======
-import '@/styles/globals.css';
-import '@/styles/login.css'; 
-import '@/styles/func_dashboard.css';
-import '@/styles/func_timetracker.css';
-import '@/styles/sidebar.css';
-import Head from 'next/head';
-
-export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <link 
-          rel="stylesheet" 
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" 
-          crossOrigin="anonymous"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
-}
->>>>>>> 531ceaf518ea6734da3d040e62bfcc1eb7ae4cf4
